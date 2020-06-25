e577624fa2dd303d93adb5793b984a0db9b1917d
Pieter Callewaert <pieter@nephroflow.com>
2020-06-25 17:07:33 +0200

    use correct path for input file html

--- xunit-visualizer-to-pdf/entrypoint.sh
+++ xunit-visualizer-to-pdf/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
 xunit-viewer --results=$1 --output=testrun.html
-chrome-headless-render-pdf --url=file:///testrun.html --pdf testrun.pdf --chrome-option=--no-sandbox --include-background
+chrome-headless-render-pdf --url=file:///github/workspace/testrun.html --pdf testrun.pdf --chrome-option=--no-sandbox --include-background
