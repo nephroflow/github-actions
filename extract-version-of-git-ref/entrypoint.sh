8904fa568387952082ff091e12114c4e47ab676b
Pieter Callewaert <pieter.callewaert@nipro-group.com>
2021-03-31 08:49:22 +0200

    Remove obsolete actions

--- extract-version-of-git-ref/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -l
-
-echo "Input: $1"
-output=$(echo "$1" | sed "s#refs/tags/##g")
-echo "Output: $output"
-
-echo "::set-output name=version::$output"
--- extract-version-of-package-json/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -l
-
-output=v$(jq -r .version package.json)-dev
-echo "Output: $output"
-
-echo "::set-output name=version::$output"
